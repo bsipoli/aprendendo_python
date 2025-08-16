e0afd178cc98b28b54f6ada1f430e8719bbb63ab
bsipoli <99055764+bsipoli@users.noreply.github.com>
2025-08-16 02:12:11 -0300

    Update variaveis.py

--- variaveis.py
+++ variaveis.py
@@ -3,5 +3,6 @@ ano_nascimento = 1986
 cidade = "São Paulo"
 print("Olá, meu nome é " + nome_usuario + ", nasci em " + str(ano_nascimento) + " e moro em " + cidade)
 
-    #USA str() por que nao podemos misturar textos(string) com números(float) diretamento na mesma linha de código.
+    #Usa str() por que nao podemos misturar textos(string) com números(float) diretamento na mesma linha de código.
+
     #A função str() converte o número em um texto para que possamos juntá-los.
